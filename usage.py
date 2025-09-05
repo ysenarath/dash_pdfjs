539baf0cebb585444b627b5795056d84ff75bf56
Yasas Senarath <12231659+ysenarath@users.noreply.github.com>
2025-09-05 10:59:17 -0400

    refactor: format configuration files and improve layout in usage.py

--- usage.py
+++ usage.py
@@ -3,20 +3,18 @@ from dash import Dash, callback, html, Input, Output
 
 app = Dash(__name__)
 
-app.layout = html.Div([
-    dash_pdfjs.DashPdfjs(
-        id='input',
-        value='my-value',
-        label='my-label'
-    ),
-    html.Div(id='output')
-])
+app.layout = html.Div(
+    [
+        dash_pdfjs.DashPdfjs(id="input", value="my-value", label="my-label"),
+        html.Div(id="output"),
+    ]
+)
 
 
-@callback(Output('output', 'children'), Input('input', 'value'))
+@callback(Output("output", "children"), Input("input", "value"))
 def display_output(value):
-    return 'You have entered {}'.format(value)
+    return "You have entered {}".format(value)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
